11e86a22822be06b2eb7eaecef630c3aaeb2c720
Bacteriafish <you@example.com>
2020-01-15 15:44:00 +0800

    Initial Commit

---
+++ Auto Layout Event Cycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Event Cycle
+//
+//  Created by FISH on 2020/1/15.
+//  Copyright © 2020 FISH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
